da9d9bd9eab9e423e256d805e7b2f2d66f34362b
usmanovbf <usmanovbf@yandex.ru>
2017-04-27 11:51:37 +0300

    Correct HTTP status code

--- raspberry-pi-client/src/main/java/com/github/usmanovbf/raspberry/pi/client/sender/RestSender.java
+++ raspberry-pi-client/src/main/java/com/github/usmanovbf/raspberry/pi/client/sender/RestSender.java
@@ -55,21 +55,22 @@ public class RestSender {
 
             ClientResponse<String> response = request.post( String.class );
 
-            if (response.getStatus() != 201) {
+            if (response.getStatus() != 201 || response.getStatus() != 200) {
                 throw new RuntimeException( "Failed : HTTP error code : "
                         + response.getStatus() );
             }
 
-            BufferedReader br = new BufferedReader( new InputStreamReader(
-                    new ByteArrayInputStream( response.getEntity().getBytes() ) ) );
+            try (BufferedReader br = new BufferedReader( new InputStreamReader(
+                    new ByteArrayInputStream( response.getEntity().getBytes() ) ) )) {
 
-            String output;
-            logger.info( "Output from Server .... \n" );
-            while (( output = br.readLine() ) != null) {
+                String output;
+                logger.info( "Output from Server .... \n" );
+                while (( output = br.readLine() ) != null) {
 
-                logger.info( output );
+                    logger.info( output );
+                }
             }
-
+            response.close();
         } catch (Exception e) {
 
             logger.error( "Error", e );
