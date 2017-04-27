9468da961b7c577ba2b81e1ca72f9db254b15f8b
usmanovbf <usmanovbf@yandex.ru>
2017-04-27 12:05:45 +0300

    static initialization of Platform 2

--- raspberry-pi-client/src/main/java/com/github/usmanovbf/raspberry/pi/client/Starter.java
+++ raspberry-pi-client/src/main/java/com/github/usmanovbf/raspberry/pi/client/Starter.java
@@ -1,12 +1,26 @@
 package com.github.usmanovbf.raspberry.pi.client;
 
+import com.github.usmanovbf.raspberry.pi.client.collector.Collector;
 import com.github.usmanovbf.raspberry.pi.client.sender.RestSender;
+import com.pi4j.platform.Platform;
+import com.pi4j.platform.PlatformAlreadyAssignedException;
+import com.pi4j.platform.PlatformManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Starter {
+    private static Logger logger = LoggerFactory.getLogger( Collector.class );
+
     public static void main( String[] args ) {
+        try {
+            PlatformManager.setPlatform( Platform.RASPBERRYPI );
+        } catch (PlatformAlreadyAssignedException e) {
+            logger.error( "Error while setting platform version", e );
+        }
+
         int delay = 5000;   // delay for 5 sec.
         int interval = 1000;  // iterate every sec.
         Timer timer = new Timer();
--- raspberry-pi-client/src/main/java/com/github/usmanovbf/raspberry/pi/client/collector/Collector.java
+++ raspberry-pi-client/src/main/java/com/github/usmanovbf/raspberry/pi/client/collector/Collector.java
@@ -1,8 +1,5 @@
 package com.github.usmanovbf.raspberry.pi.client.collector;
 
-import com.pi4j.platform.Platform;
-import com.pi4j.platform.PlatformAlreadyAssignedException;
-import com.pi4j.platform.PlatformManager;
 import com.pi4j.system.SystemInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,21 +7,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 public class Collector {
-    private static Logger logger = LoggerFactory.getLogger( Collector.class );
-
-
-    static {
-        try {
-            PlatformManager.setPlatform( Platform.RASPBERRYPI );
-        } catch (PlatformAlreadyAssignedException e) {
-            logger.error( "Error while setting platform version", e );
-        }
-
-    }
-
-
-    public Collector() {
-    }
+    private Logger logger = LoggerFactory.getLogger( Collector.class );
 
     public float getTemperature() {
         float cpuTemperature = 40;
